b2784063e07e9b54f3c986944aeff2ab8a850ba6
whammou@arch <khoabui.ld99@gmail.com>
2025-09-26 17:41:58 +0700

    test module

--- install.sh
+++ install.sh
@@ -3,8 +3,5 @@
 current_dir=$(pwd)
 
 ln -s "$current_dir/modules/languages" -t "$current_dir"
-echo "languages" >>$current_dir/.gitignore
 ln -s "$current_dir/modules/teaching" -t "$current_dir/languages"
-echo "teaching" >>$current_dir/languages/.gitignore
 ln -s "$current_dir/modules/academic" -t "$current_dir/languages/teaching"
-echo "academic" >>$current_dir/languages/teaching/.gitignore
